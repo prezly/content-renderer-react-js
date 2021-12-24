0f342af451d08a6ecb83b0bdcaa272e112b74bb2
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-12-24 15:10:24 +0200

    Configure `build:sass` and `watch:sass` targets

---
+++ gulpfile.mjs
@@ -0,0 +1,72 @@
+// General imports
+import autoprefixer from 'autoprefixer';
+import branch from 'branch-pipe';
+import gulp from 'gulp';
+import sassBackend from 'sass';
+import { Transform } from 'stream';
+
+// Processors
+import concat from 'gulp-concat';
+import filter from 'gulp-filter';
+import map from 'gulp-map';
+import rename from 'gulp-rename';
+import postcss from 'gulp-postcss';
+import createSassProcessor from 'gulp-sass';
+
+const sass = createSassProcessor(sassBackend);
+
+const SASS_SOURCES = 'src/**/*.scss';
+const SASS_DECLARATIONS = 'src/styles/**/*.scss';
+const SASS_MODULES_STYLESHEETS = ['src/**/*.scss', '!src/styles/**/*.scss'];
+
+gulp.task('build:sass', function () {
+    return gulp
+        .src(SASS_SOURCES)
+        .pipe(
+            branch.obj((src) => [
+                copySassDeclarations(src),
+                compileComponentsStylesheets(src),
+            ]),
+        )
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('watch:sass', gulp.series('build:sass', function () {
+    return gulp
+        .watch(SASS_SOURCES, gulp.series('build:sass'))
+        .on('ready', () => console.log('Watching files'))
+        .on('all', (event, path) => console.log(`[${event}] ${path}`));
+}));
+
+/**
+ * @param {Transform} stream
+ * @returns {Transform}
+ */
+function copySassDeclarations(stream) {
+    return stream.pipe(filter(SASS_DECLARATIONS));
+}
+
+/**
+ * @param {Transform} stream
+ * @returns {Transform}
+ */
+function compileComponentsStylesheets(stream) {
+    /**
+     * @param {vinyl:File} file
+     * @returns {vinyl:File}
+     */
+    function toSassIndex(file) {
+        const path = file.path.replace(file.base, '.').replace(/\.scss$/, '');
+        const index = file.clone({ contents: false });
+        index.contents = Buffer.from(`@import "${path}";\n`);
+        return index;
+    }
+
+    return stream
+        .pipe(filter(SASS_MODULES_STYLESHEETS))
+        .pipe(map(toSassIndex))
+        .pipe(concat('styles.scss'))
+        .pipe(sass({ importPath: 'src/' }))
+        .pipe(postcss([autoprefixer({ grid: true })]))
+        .pipe(rename({ extname: '.css', dirname: 'styles/' }));
+}
