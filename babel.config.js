6ced65256e52604e3cfc391a6fb1ef2a7f97e61d
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2021-12-24 18:13:10 +0200

    Fix failing tests

--- babel.config.js
+++ babel.config.js
@@ -4,22 +4,41 @@ module.exports = (api) => {
 
     const isProduction = api.env('production');
     const isDevelopment = api.env('development');
+    const isTest = api.env('testing');
 
     return {
         targets: {
             esmodules: false,
             node: '12',
         },
+
         presets: [
             '@babel/typescript',
             ['@babel/react', { development: !isProduction, runtime: 'automatic' }],
             '@dr.pogodin/babel-preset-svgr',
-            ['@babel/env', { useBuiltIns: false,  modules: 'commonjs' }],
+            ['@babel/env', {
+                useBuiltIns: false,
+                modules: 'commonjs',
+                targets: isTest ? { esmodules: false } : undefined,
+            }],
         ],
+
         plugins: [
             ['babel-plugin-transform-remove-imports', { test: '\\.scss$' }],
             // Applies the react-refresh Babel plugin on non-production modes only
             isDevelopment && 'react-refresh/babel',
         ].filter(Boolean),
+
+        overrides: [
+            {
+                test: './node_modules/@prezly/**/*.mjs',
+                plugins: [
+                    [
+                        '@babel/plugin-transform-modules-commonjs',
+                        { importInterop: 'none', strict: true },
+                    ],
+                ],
+            }
+        ],
     };
 };
--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,15 @@ module.exports = {
             tsconfig: 'tsconfig.jest.json',
         },
     },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs', 'jsx', 'json'],
     moduleNameMapper: {
         '\\.(scss|svg)$': '<rootDir>/universalMock',
     },
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     testPathIgnorePatterns: ['/node_modules/', '/build/'],
+    transform: {
+        '^.+\\.(ts|tsx|js|jsx|mjs)$': 'babel-jest',
+    },
+    transformIgnorePatterns: ['/node_modules/(?!@prezly/slate-)(.*)/'],
 };
