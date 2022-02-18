5261c2788786b73fd3a648eb403e3c4b59deb6f5
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-02-18 14:52:49 +0200

    Link Image Rollover components to the original image file

--- src/elements/Gallery/GalleryImage.tsx
+++ src/elements/Gallery/GalleryImage.tsx
@@ -28,7 +28,7 @@ export function GalleryImage({
                 'prezly-slate-gallery-image--with-border-radius': rounded,
             })}
             caption={originalImage.caption}
-            href="#"
+            href={originalImage.rawCdnUrl}
             onClick={() => onClick(originalImage)}
             style={style}
         >
--- src/elements/Image/Image.tsx
+++ src/elements/Image/Image.tsx
@@ -70,7 +70,7 @@ export const Image: FunctionComponent<Props> = ({
                 <Rollover
                     id={`image-${file.uuid}`}
                     disabled={image.isGif()}
-                    href="#"
+                    href={image.rawCdnUrl}
                     onClick={handleRolloverClick}
                     style={containerStyle}
                 >
