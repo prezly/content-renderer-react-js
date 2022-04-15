261a89569362e117eb786e54bf23a95709d9a10e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-04-15 18:09:02 +0300

    Simplify `Selector` inner implementation

--- src/selector/Selector.tsx
+++ src/selector/Selector.tsx
@@ -1,71 +1,36 @@
-import type { Node } from 'slate';
 import type { ReactNode } from 'react';
-import React, { Fragment, useCallback, useEffect, useState } from 'react';
-import { ComponentRenderer, createComponentsRenderersFromChildren } from './lib';
+import React, { Fragment } from 'react';
+import type { Node } from 'slate';
 import { Element } from 'slate';
+
+import { createComponentsRenderersFromChildren } from './lib';
 
 interface Props {
     nodes: Node | Node[];
     children: ReactNode;
 }
 
-export function Selector(props: Props) {
-    const renderers = useStableRenderers(createComponentsRenderersFromChildren(props.children));
+export function Selector({ nodes, children }: Props) {
+    const renderers = createComponentsRenderersFromChildren(children);
 
-    const render = useCallback(
-        function (nodes: Node | Node[]): ReactNode {
-            function renderNodes(nodes: Node[]): ReactNode {
-                return nodes.map((node, idx) => <Fragment key={idx}>{renderNode(node)}</Fragment>);
+    function renderNodes(nodes: Node[]): ReactNode {
+        return nodes.map((node, idx) => <Fragment key={idx}>{renderNode(node)}</Fragment>);
+    }
+
+    function renderNode(node: Node) {
+        for (const { match, component: ComponentRenderer, ...extraProps } of renderers) {
+            if (match(node)) {
+                return (
+                    <ComponentRenderer
+                        {...extraProps}
+                        node={node}
+                        children={Element.isElement(node) ? renderNodes(node.children) : undefined}
+                    />
+                );
             }
+        }
+        return null;
+    }
 
-            function renderNode(node: Node) {
-                for (const { match, component: ComponentRenderer, ...extraProps } of renderers) {
-                    if (match(node)) {
-                        return (
-                            <ComponentRenderer
-                                {...extraProps}
-                                node={node}
-                                children={
-                                    Element.isElement(node) ? renderNodes(node.children) : undefined
-                                }
-                            />
-                        );
-                    }
-                }
-                return null;
-            }
-
-            return Array.isArray(nodes) ? renderNodes(nodes) : renderNode(nodes);
-        },
-        [renderers],
-    );
-
-    return <>{render(props.nodes)}</>;
-}
-
-function useStableRenderers(renderers: ComponentRenderer[]) {
-    const [prevRenderers, setPrevRenderers] = useState<ComponentRenderer[]>(renderers);
-
-    useEffect(
-        function () {
-            if (!isRendererArrayEqual(prevRenderers, renderers)) {
-                setPrevRenderers(renderers);
-            }
-        },
-        [renderers],
-    );
-
-    return prevRenderers;
-}
-
-function isRendererArrayEqual(a: ComponentRenderer[], b: ComponentRenderer[]): boolean {
-    return a.length === b.length && a.every((_, i) => isRendererEqual(a[i], b[i]));
-}
-
-function isRendererEqual(a: ComponentRenderer, b: ComponentRenderer): boolean {
-    return (
-        a.match === b.match &&
-        a.component === b.component &&
-        JSON.stringify(a.extraProps) === JSON.stringify(b.extraProps)
-    );
+    return <>{Array.isArray(nodes) ? renderNodes(nodes) : renderNode(nodes)}</>;
 }
