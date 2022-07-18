23045cb690aa374f946323ede6740189283af180
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-07-18 16:44:31 +0300

    Use snapshots instead of `ReactDOMServer.renderToString()`

--- src/selector/Selector.test.tsx
+++ src/selector/Selector.test.tsx
@@ -1,6 +1,6 @@
 import { DividerNode, DocumentNode, HeadingNode } from '@prezly/story-content-format';
 import React from 'react';
-import ReactDOMServer from 'react-dom/server.js';
+import renderer from 'react-test-renderer';
 
 import { Component } from './Component';
 import { Selector } from './Selector';
@@ -21,16 +21,17 @@ const documentNode: DocumentNode = {
 };
 
 describe('Renderer', () => {
-    it('Renders a <h1> for a heading and a <section> for a divider', () => {
-        const asString = ReactDOMServer.renderToString(
-            <Selector nodes={documentNode}>
-                <Component match={DividerNode.isDividerNode} component={Divider} />
-                <Component match={HeadingNode.isHeadingNode} component={Heading} />
-                <Component match={DocumentNode.isDocumentNode} component={Document} />
-            </Selector>,
-        );
+    it('should render a <h1> for a heading and a <section> for a divider', () => {
+        const tree = renderer
+            .create(
+                <Selector nodes={documentNode}>
+                    <Component match={DividerNode.isDividerNode} component={Divider} />
+                    <Component match={HeadingNode.isHeadingNode} component={Heading} />
+                    <Component match={DocumentNode.isDocumentNode} component={Document} />
+                </Selector>,
+            )
+            .toJSON();
 
-        expect(asString).toContain('<h1');
-        expect(asString).toContain('<section');
+        expect(tree).toMatchSnapshot();
     });
 });
