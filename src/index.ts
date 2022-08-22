e37267c17ca3c73b11e485ecda1c1fe6ead39309
OskarGasiorowski <41072665+OskarGasiorowski@users.noreply.github.com>
2022-08-22 16:05:18 +0200

    [QRF-26] Fixed not supported ResizeObserver in Safari 12 (#38)
    * [QRF-26] Fixed ResizeObserver issue in Safari 12
    
    * linter fix
    
    * Moved ResizeObserver polyfill override to index.ts and use polyfill only when ResizeObserver is undefined
    
    * Adding polyfill only when window is not undefined
    
    * fixed lockfileVersion package-lock.json
    
    * Update src/index.ts
    
    Co-authored-by: Ivan Voskoboinyk <ivan@voskoboinyk.com>
    
    Co-authored-by: Ivan Voskoboinyk <ivan@voskoboinyk.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import ResizeObserver from 'resize-observer-polyfill';
+
 export type { Transformation } from './types';
 
 export * as Elements from './elements';
@@ -6,3 +8,7 @@ export { Component, Selector } from './selector';
 export * as Transformations from './transformations';
 
 export { Renderer } from './Renderer';
+
+if (typeof window !== 'undefined') {
+    window.ResizeObserver = window.ResizeObserver ?? ResizeObserver;
+}
