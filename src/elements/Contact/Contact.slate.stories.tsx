5af93cfbaba7fcd97d5df5f04af00f15b2219688
kudlajz <kudlajzzz@gmail.com>
2023-05-23 13:13:12 +0200

    Revert Contact story changes

--- src/elements/Contact/Contact.slate.stories.tsx
+++ src/elements/Contact/Contact.slate.stories.tsx
@@ -13,6 +13,11 @@ export const PressContacts: Story = () => (
         nodes={[
             {
                 type: 'contact',
+                children: [
+                    {
+                        text: '',
+                    },
+                ],
                 uuid: 'f860590b-db97-44de-a59c-12848956b559',
                 reference: 'a456b750-65cb-49da-8f4a-7f53906bce28',
                 contact: {
