f560ff60f4cf35490518fd7c50cc162579da23ae
Oleg Semyonov <oleg@prezly.com>
2023-05-29 10:49:11 +0400

    Re-run ESLint

--- src/elements/Contact/Contact.slate.stories.tsx
+++ src/elements/Contact/Contact.slate.stories.tsx
@@ -54,7 +54,8 @@ export const CardWithoutAvatar: Story = () => (
                 uuid: 'f860590b-db97-44de-a59c-12848956b559',
                 reference: 'a456b750-65cb-49da-8f4a-7f53906bce28',
                 contact: {
-                    avatar_url: 'https://ucarecdn.com/da3ea88f-ddfe-4593-a6e4-07c9f9467d5e/PhilipCooper.jpg',
+                    avatar_url:
+                        'https://ucarecdn.com/da3ea88f-ddfe-4593-a6e4-07c9f9467d5e/PhilipCooper.jpg',
                     company: 'TechCrunch',
                     description: 'PR Manager',
                     email: 'phil@techcrunch.com',
@@ -86,7 +87,8 @@ export const Signature: Story = () => (
                 uuid: 'f860590b-db97-44de-a59c-12848956b559',
                 reference: 'a456b750-65cb-49da-8f4a-7f53906bce28',
                 contact: {
-                    avatar_url: 'https://ucarecdn.com/da3ea88f-ddfe-4593-a6e4-07c9f9467d5e/PhilipCooper.jpg',
+                    avatar_url:
+                        'https://ucarecdn.com/da3ea88f-ddfe-4593-a6e4-07c9f9467d5e/PhilipCooper.jpg',
                     company: 'TechCrunch',
                     description: 'PR Manager',
                     email: 'phil@techcrunch.com',
@@ -118,7 +120,8 @@ export const SignatureWithoutAvatar: Story = () => (
                 uuid: 'f860590b-db97-44de-a59c-12848956b559',
                 reference: 'a456b750-65cb-49da-8f4a-7f53906bce28',
                 contact: {
-                    avatar_url: 'https://ucarecdn.com/da3ea88f-ddfe-4593-a6e4-07c9f9467d5e/PhilipCooper.jpg',
+                    avatar_url:
+                        'https://ucarecdn.com/da3ea88f-ddfe-4593-a6e4-07c9f9467d5e/PhilipCooper.jpg',
                     company: 'TechCrunch',
                     description: 'PR Manager',
                     email: 'phil@techcrunch.com',
