d88258ea6d569ed8e23c270a61e080e20dc1745d
Francis Gyimah <fgyimah883@gmail.com>
2023-08-11 08:59:39 +0000

    [DEV-11239] Prevent button render if no href, updated classname to match layout

--- src/elements/ButtonBlock/ButtonBlock.tsx
+++ src/elements/ButtonBlock/ButtonBlock.tsx
@@ -10,12 +10,15 @@ interface ButtonNodeProps {
 export function ButtonBlock({ node }: ButtonNodeProps) {
     const { href, label, layout, new_tab: newTab, variant } = node;
 
+    if (!href) {
+        return null;
+    }
+
     return (
         <a
             className={classNames('prezly-slate-button-block', {
-                'prezly-slate-button-block--disabled': !href,
                 'prezly-slate-button-block--outlined': variant === ButtonBlockNode.Variant.OUTLINE,
-                'prezly-slate-button-block--fullWidth': layout === ButtonBlockNode.Layout.WIDE,
+                'prezly-slate-button-block--wide': layout === ButtonBlockNode.Layout.WIDE,
                 'prezly-slate-button-block--right': layout === ButtonBlockNode.Layout.RIGHT,
                 'prezly-slate-button-block--left': layout === ButtonBlockNode.Layout.LEFT,
                 'prezly-slate-button-block--center': layout === ButtonBlockNode.Layout.CENTER,
