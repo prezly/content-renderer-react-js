10f93cacec1016fa2b7cc39b37af4a8d1c097eea
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-08-28 16:47:03 +0330

    [DEV-11478] Make controls expanded in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,7 +4,12 @@ import { importAll } from '../src/dev/importAll';
 importAll(require.context('../src', true, /\.scss$/));
 
 export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: {
+        argTypesRegex: '^on[A-Z].*',
+        controls: {
+            expanded: true,
+        },
+    },
 };
 
 export const decorators = [
