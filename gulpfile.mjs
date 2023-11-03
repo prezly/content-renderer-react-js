464ad91a22c99f1f5763948728f355fb49efa2f2
kudlajz <kudlajzzz@gmail.com>
2023-11-03 13:07:38 +0100

    Fixed sass import

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,7 +2,7 @@
 import autoprefixer from 'autoprefixer';
 import branch from 'branch-pipe';
 import gulp from 'gulp';
-import * as sassBackend from 'sass';
+import sassBackend from 'sass';
 import { Transform } from 'stream';
 
 // Processors
