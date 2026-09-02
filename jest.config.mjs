4b24480f9a62cf4eeaf681e732bf69b7cd29ab8f
Francis Gyimah <fgyimah883@gmail.com>
2026-09-02 10:40:24 +0000

    [DEV-24046] Modernized TypeScript and Jest tooling

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,18 +1,16 @@
+import { createDefaultEsmPreset } from 'ts-jest';
+
+const preset = createDefaultEsmPreset({
+    tsconfig: 'tsconfig.jest.json',
+});
+
 export default {
+    ...preset,
     coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
-    extensionsToTreatAsEsm: ['.ts', '.tsx'],
-    globals: {
-        'ts-jest': {
-            type: 'module',
-            tsconfig: 'tsconfig.jest.json',
-            useESM: true,
-        },
-    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs', 'jsx', 'json'],
     moduleNameMapper: {
         '\\.(scss|svg)$': '<rootDir>/universalMock.mjs',
     },
-    preset: 'ts-jest',
     testEnvironment: 'jsdom',
     testPathIgnorePatterns: ['/node_modules/', '/build/'],
 };
